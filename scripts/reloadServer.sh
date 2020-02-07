7a086f556cb1c55087b43531d5f482ad1fb9dc4c
TheForeverLost <alanpjohn@outlook.com>
2020-02-07 23:10:50 +0530

    build

--- scripts/reloadServer.sh
+++ scripts/reloadServer.sh
@@ -1,5 +1,7 @@
 sudo rsync -r --delete-excluded --force /home/ubuntu/http-temp/ /home/ubuntu/http-server
 sudo rm -rf /home/ubuntu/http-temp 
-pm2 /home/ubuntu/http-server/restart index.js
+cd /home/ubuntu/http-server
+npm i 
 cd /home/ubuntu
+pm2 /home/ubuntu/http-server/restart index.js
 pm2 save
