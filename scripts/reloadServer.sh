9a83b15ccc47bf3f3426980c5b9af18286cb913f
TheForeverLost <alanpjohn@outlook.com>
2020-02-09 12:31:44 +0530

    reload server script repair

--- scripts/reloadServer.sh
+++ scripts/reloadServer.sh
@@ -3,5 +3,5 @@ sudo rm -rf /home/ubuntu/http-temp
 cd /home/ubuntu/http-server
 npm i 
 cd /home/ubuntu
-pm2 /home/ubuntu/http-server/restart index.js
+pm2 restart /home/ubuntu/http-server/index.js
 pm2 save
