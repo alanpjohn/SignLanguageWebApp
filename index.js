8c2f43793990c08f850a33fe320d762965a867ab
TheForeverLost <alanpjohn@outlook.com>
2020-02-23 01:05:59 +0530

    fuck these sockets

--- index.js
+++ index.js
@@ -121,7 +121,7 @@ server.listen(3000);
 // WARNING: app.listen(80) will NOT work here!
 try{
 io.on('connection', (socket) => {
-
+    console.log("connection")
     let userNickname = "test"
     socket.emit('news' , "hello user");
 
