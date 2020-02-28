6194fc0540518ba6740f8b0e155b384dfd9c29fe
TheForeverLost <alanpjohn@outlook.com>
2020-02-28 10:04:37 +0530

    facing mode environment

--- scripts/reloadServer.sh
+++ scripts/reloadServer.sh
@@ -3,6 +3,7 @@ sudo rm -rf /home/ubuntu/http-temp
 cd /home/ubuntu/http-server
 grep -rl 'http://localhost:3000' index.html | xargs sed -i 's;http://localhost:3000;https://www.clawpro.club;g'
 npm i 
+grep -rl "facingMode: 'user'" node_modules/@teachablemachine/image/src/utils/webcam.ts | xargs sed -i "s;facingMode: 'user';facingMode: { exact: 'environment' };g"
 cd /home/ubuntu
 pm2 flush
 pm2 restart /home/ubuntu/http-server/index.js
